97de6b05e318ae1df2c34a92d1cb69a5faaf8069
Luis Chang <luis.chang@outlook.com>
2024-04-13 12:28:23 -0500

    Se agregaron nuevos sistemas operativos

--- app/src/main/java/dev/luischang/listviewue/MainActivity.kt
+++ app/src/main/java/dev/luischang/listviewue/MainActivity.kt
@@ -13,7 +13,8 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
     var arrSO = arrayOf("Android",
         "iPhone","Windows Phone","Blackberry OS",
-        "Firefox OS","Amazon World","Nokia")
+        "Firefox OS","Amazon World","Nokia",
+        "Sistema Operativo 1", "Sistema Operativo 2")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
